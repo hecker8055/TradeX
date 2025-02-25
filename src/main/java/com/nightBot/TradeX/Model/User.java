6f62747c7efa02825017cff7f3e3b1f16691f52a
hecker8055 <146121211+hecker8055@users.noreply.github.com>
2025-02-25 23:05:53 +0530

    Domain And Repository created

--- src/main/java/com/nightBot/TradeX/Model/User.java
+++ src/main/java/com/nightBot/TradeX/Model/User.java
@@ -1,4 +1,30 @@
 package com.nightBot.TradeX.Model;
 
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.nightBot.TradeX.Domain.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+
+    private String fullname;
+    private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @Embedded
+    private TwoFactorAuth  twoFactorAuth = new TwoFactorAuth();
+
+    private UserRole userRole = UserRole.RoleCustomer;
+
+
 }
