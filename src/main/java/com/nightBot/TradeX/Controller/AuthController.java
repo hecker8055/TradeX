a3f81a1aedcc6dda76a83b2893474163bf7d9a7b
hecker8055 <146121211+hecker8055@users.noreply.github.com>
2025-03-12 13:34:29 +0530

    Authentication Ant Authorization

---
+++ src/main/java/com/nightBot/TradeX/Controller/AuthController.java
@@ -0,0 +1,118 @@
+package com.nightBot.TradeX.Controller;
+
+import com.nightBot.TradeX.Config.JwtProvider;
+import com.nightBot.TradeX.Model.TwoFactorOTP;
+import com.nightBot.TradeX.Model.User;
+import com.nightBot.TradeX.Repository.UserRepository;
+
+import com.nightBot.TradeX.Response.AuthResponse;
+import com.nightBot.TradeX.Service.CustomUserDetailService;
+import com.nightBot.TradeX.Service.TwoFactorOtpService;
+import com.nightBot.TradeX.Utils.OtpUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
+
+    @Autowired
+    private TwoFactorOtpService twoFactorOtpService;
+
+    @PostMapping("/signUp")
+    public ResponseEntity<AuthResponse> register(@RequestBody User user) throws Exception {
+
+
+        User isEmailExist = userRepository.findByEmail(user.getEmail());
+
+        if(isEmailExist != null){
+            throw new Exception("Email already exists");
+        }
+        User newUser = new  User();
+        newUser.setEmail(user.getEmail());
+        newUser.setPassword(user.getPassword());
+        newUser.setFullName(user.getFullName());
+
+
+        User savedUser = userRepository.save(newUser);
+
+        Authentication auth= new UsernamePasswordAuthenticationToken(
+                user.getEmail(),
+                user.getPassword()
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        String jwt = JwtProvider.generateToken(auth);
+
+        if(user.getTwoFactorAuth().isEnabled()){
+            AuthResponse res = new AuthResponse();
+            res.setMessage("Two factor auth is enabled");
+            res.setTwoFactorAuthEnable(true);
+            String otp = OtpUtils.generateOTP();
+
+            TwoFactorOTP oldTwoFactorOTP = twoFactorOtpService.findByUser(user.getId());
+
+        }
+
+        AuthResponse res = new AuthResponse();
+        res.setJwt(jwt);
+        res.setStatus(true);
+        res.setMessage("register Success");
+
+
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
+
+    }
+    @PostMapping("/signIn")
+    public ResponseEntity<AuthResponse> login(@RequestBody User user) throws Exception {
+
+        String userName = user.getEmail();
+        String password = user.getPassword();
+        Authentication auth = authenticate(userName,password);
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        String jwt = JwtProvider.generateToken(auth);
+
+        AuthResponse res = new AuthResponse();
+        res.setJwt(jwt);
+        res.setStatus(true);
+        res.setMessage("login Success");
+
+
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
+
+    }
+
+    private Authentication authenticate(String userName, String password) {
+           UserDetails userDetails = customUserDetailService.loadUserByUsername(userName);
+
+           if(userDetails==null){
+               throw new BadCredentialsException("Invalid username");
+           }
+           if(!password.equals(userDetails.getPassword())){
+               throw new BadCredentialsException("Invalid password");
+           }
+
+           return new UsernamePasswordAuthenticationToken(userDetails,password,userDetails.getAuthorities());
+    }
+}
