62f5daf6a72f7f63a3203a450d01d8e4027fa829
hecker8055 <146121211+hecker8055@users.noreply.github.com>
2025-03-18 20:59:32 +0530

    forgot password and update

--- src/main/java/com/nightBot/TradeX/Service/VerificationCodeService.java
+++ src/main/java/com/nightBot/TradeX/Service/VerificationCodeService.java
@@ -1,14 +1,18 @@
 package com.nightBot.TradeX.Service;
 
+import com.nightBot.TradeX.Domain.VerificationType;
+import com.nightBot.TradeX.Model.User;
 import com.nightBot.TradeX.Model.VerificationCode;
 
 public interface VerificationCodeService {
 
-    VerificationCode sendVerificationCode(VerificationCode verificationCode);
+    VerificationCode sendVerificationCode(User user , VerificationType verificationType);
 
-    VerificationCode getVerificationCodeById(Long Id);
+    VerificationCode getVerificationCodeById(Long id) throws Exception;
 
     VerificationCode getVerificationCodeByUser(Long userId);
+
+
 
     void deleteVerificationCodeById(VerificationCode verificationCode);
 
